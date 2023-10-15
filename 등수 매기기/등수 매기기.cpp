990bc77017b1fe10f044307599963eab5ada2bf0
mooneda9999 <108799051+mooneda9999@users.noreply.github.com>
2023-10-15 15:05:15 +0900

    Add files via upload

---
+++ 등수 매기기/등수 매기기.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main(){
+	int N, x;
+	vector<int> v, n;
+	scanf("%d", &N);
+	for(int i = 0; i < N;  i++){
+		scanf("%d", &x);
+		v.push_back(x);
+	}
+	for(int j = 0; j < N - 1;  j++){
+		for(int k = 0; k < N - 1;  k++){
+			if()
+		}
+	}
+	
+}
